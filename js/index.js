3ec8a55074e186515f4dde2a9798c8c8007226b9
Jochaz <jordan.chariot@gmail.com>
2025-03-13 22:58:33 +0100

    Ajout des infos du serveur

--- js/index.js
+++ js/index.js
@@ -11,3 +11,19 @@ function toggleMenu() {
     const menu = document.getElementById("mobileMenu");
     menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const background = document.querySelector(".background-infos");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                background.classList.add("visible");
+            } else {
+                background.classList.remove("visible"); // Disparition quand il sort de l'écran
+            }
+        });
+    }, { threshold: 0.3 }); // Déclenche l'animation quand 30% de .background est visible
+
+    observer.observe(background);
+});
